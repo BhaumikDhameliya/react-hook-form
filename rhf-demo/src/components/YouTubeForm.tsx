5f539bf5bda68228ffad2eb26e826e7330d13f5a
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-04-07 10:40:03 +0530

    devtool visualization

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -1,10 +1,9 @@
-import React from "react";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 export default function YouTubeForm() {
   const form = useForm();
-  const { register } = form;
-  const { name, ref, onChange, onBlur } = register("username");
+  const { register, control } = form;
 
   return (
     <div>
@@ -20,6 +19,7 @@ export default function YouTubeForm() {
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 }
