86038b2f076d89ecd688949af691a34a2cdf1487
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-04-17 16:39:20 +0530

    dynamic fields(useFieldArray)

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -1,4 +1,4 @@
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 
 type FormValues = {
@@ -10,6 +10,9 @@ type FormValues = {
     facebook: String;
   };
   phoneNumbers: string[];
+  phNumbers: {
+    number: string;
+  }[];
 };
 
 export default function YouTubeForm() {
@@ -23,6 +26,7 @@ export default function YouTubeForm() {
         facebook: "",
       },
       phoneNumbers: ["", ""],
+      phNumbers: [{ number: "" }],
     },
 
     // we can also pass function as a default values
@@ -40,6 +44,11 @@ export default function YouTubeForm() {
   });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
+
+  const { fields, append, remove } = useFieldArray({
+    name: "phNumbers",
+    control: control,
+  });
 
   const onSubmit = (data: FormValues) => {
     console.log("Form submitted", data);
@@ -134,6 +143,30 @@ export default function YouTubeForm() {
             id="secondary-phone"
             {...register("phoneNumbers.1")}
           />
+        </div>
+
+        <div>
+          <label>List of phone numbers</label>
+          <div>
+            {fields.map((field, index) => {
+              return (
+                <div className="form-control" key={field.id}>
+                  <input
+                    type="text"
+                    {...register(`phNumbers.${index}.number`)}
+                  />
+                  {index > 0 && (
+                    <button type="button" onClick={() => remove(index)}>
+                      Remove
+                    </button>
+                  )}
+                </div>
+              );
+            })}
+            <button type="button" onClick={() => append({ number: "" })}>
+              Add phone number
+            </button>
+          </div>
         </div>
 
         <button>Submit</button>
