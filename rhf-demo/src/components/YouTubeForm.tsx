87a7be68b1f934eb9b9b8d6d2cae63313330226e
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-04-17 16:16:59 +0530

    defaultValues

--- rhf-demo/src/components/YouTubeForm.tsx
+++ rhf-demo/src/components/YouTubeForm.tsx
@@ -8,7 +8,26 @@ type FormValues = {
 };
 
 export default function YouTubeForm() {
-  const form = useForm<FormValues>();
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: "Batman",
+      email: "",
+      channel: "",
+    },
+
+    // we can also pass function as a default values
+    // defaultValues: async () => {
+    //   const response = await fetch(
+    //     "https://jsonplaceholder.typicode.com/users/1"
+    //   );
+    //   const data = await response.json();
+    //   return {
+    //     username: "Batman",
+    //     email: data.email,
+    //     channel: "",
+    //   };
+    // },
+  });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
 
