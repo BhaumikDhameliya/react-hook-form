021f25bff78a4822708bd2b5049035d26eadb39b
Bhaumik Dhameliya <bhaumikdhameliya30@gmail.com>
2023-04-27 18:09:20 +0530

    MUI integration

--- rhf-demo/src/App.tsx
+++ rhf-demo/src/App.tsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
+// import "./App.css";
 import YouTubeForm from "./components/YouTubeForm";
+import MuiLoginForm from "./components/MuiLoginForm";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <YouTubeForm />
+      {/* <YouTubeForm /> */}
+      <MuiLoginForm />
     </div>
   );
 }
